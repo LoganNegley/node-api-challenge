aa46db908f232362d774aa34ae0f47c3dd846d91
Logan Negley <logannegley@gmail.com>
2020-04-17 18:42:09 -0500

    get route for projects and with project ID

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -1,11 +1,51 @@
 const express = require('express');
 const db = require('../data/helpers/projectModel');
+const actionsDB = require('../data/helpers/actionModel')
 
 const router = express.Router();
 
 
 // GET all projects
-router.get('/')
+router.get('/', (req, res) =>{
+    db.get()
+    .then(project =>{
+        res.status(200).json(project)
+    })
+    .catch(error =>{
+        console.log(error)
+        res.status(500).json({
+            errorMessage: 'The projects could not be received'
+        })
+    })
+});
+
+// GET project with ID
+router.get('/:id', validateProjectId, (req, res) =>{
+    db.get(req.params.id)
+    .then(project =>{
+        res.status(200).json(req.project)
+    })
+    .catch(error =>{
+        console.log(error)
+        res.status(500).json({
+            errorMessage: 'Was not able to retreived project with that ID'
+        })
+    })
+});
+
+// GET caction with project ID
+router.get('/:id/actions', validateProjectId, (req, res) =>{
+    db.get(req.params.id)
+    .then(project =>{
+
+    })
+    .catch(error =>{
+        console.log(error)
+        res.status(500).json({
+            errorMessage: 'Was not able to retreived actions for project'
+        })
+    })
+});
 
 
 
